1eba22a4d54a6739c4cbf090ca10fe52f3ad63e4
Carlos Alexandre <carlos.alexandre@engenharia.ufjf.br>
2017-01-25 22:13:11 -0200

    Correções finais

---
+++ config/env/production.js
@@ -0,0 +1,3 @@
+module.exports = {
+	db: "mongodb://heroku:carlalex7@ds145168.mlab.com:45168/heroku_z5bmfb5d"
+}
--- index.js
+++ index.js
@@ -1,10 +1,11 @@
-process.env.NODE_ENV = process.env.NODE_ENV||"devel";
+process.env.NODE_ENV = process.env.NODE_ENV ||"devel";
 
 var mongoose = require('./config/mongoose');
 var express = require("./config/express");
 var db = mongoose();
 var app = express();
-app.listen(3000);
-console.log("Executando na porta http://localhost:3000...");
+app.listen((process.env.PORT || 5000), function() {
+  console.log('Node app is running on port', (process.env.PORT || 5000));
+});
 
 module.exports = app;
